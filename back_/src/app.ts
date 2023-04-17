9287cbb3513f7994e8c306fe1521e5d207782331
SamRemix <s_capron@hotmail.fr>
2023-04-17 16:17:17 +0200

    cleanup

--- back_/src/app.ts
+++ back_/src/app.ts
@@ -3,19 +3,20 @@ import express from 'express'
 import { router as authRouter } from './auth/auth.route'
 import { router as userRouter } from './users/users.route'
 import { router as postRouter } from './posts/posts.route'
-import { log } from './middlewares/log'
-import error from './middlewares/errorHandler'
+
+import logRequest from './middlewares/logRequest'
+import errorHandler from './middlewares/errorHandler'
 
 const app = express()
 
 app.use(express.json())
 
-app.use(log)
+app.use(logRequest)
 
 app.use('/auth', authRouter)
 app.use('/user', userRouter)
 app.use('/post', postRouter)
 
-app.use(error)
+app.use(errorHandler)
 
 export default app
--- back_/src/middlewares/errorHandler.ts
+++ back_/src/middlewares/errorHandler.ts
@@ -1,14 +1,14 @@
 import { Prisma } from '@prisma/client'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
-  const { name, message, code, meta } = err
+  const { message, code, meta } = err
 
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
     return res.status(400).json({ code, message: meta?.cause })
   }
 
-  return res.status(400).json({ name, message })
+  return res.status(400).json({ message })
 }
 
 export default errorHandler
