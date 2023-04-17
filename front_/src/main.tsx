4a5f34826acdbb133c1a565f5bae074a124fe2df
SamRemix <s_capron@hotmail.fr>
2023-04-18 00:23:21 +0200

    basic styles

--- front_/src/main.tsx
+++ front_/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
+import './styles/globals.scss'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
