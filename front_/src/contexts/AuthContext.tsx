8eab1803079b70c797ce9ebf81ea3661eebad6a6
SamRemix <s_capron@hotmail.fr>
2023-04-19 19:09:08 +0200

    auth context

---
+++ front_/src/contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useReducer, useEffect, useState } from 'react'
+
+type AuthContextProps = {
+  token: string | null,
+  register: (token: string) => void,
+  logOut: () => void
+}
+
+type AuthProviderProps = {
+  children: React.ReactNode
+}
+
+export const AuthContext = createContext<AuthContextProps | null>(null)
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [token, setToken] = useState<string | null>(null)
+
+  const register = (token: string) => {
+    localStorage.setItem('token', token)
+
+    return setToken(token)
+  }
+
+  const logOut = () => {
+    localStorage.removeItem('token')
+
+    return setToken(null)
+  }
+
+  const store = localStorage.getItem('token')
+
+  useEffect(() => {
+    if (!store) return
+
+    setToken(store)
+  }, [store])
+
+  console.log('AUTH_CONTEXT', token)
+
+  return (
+    <AuthContext.Provider value={{ token, register, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
--- front_/src/main.tsx
+++ front_/src/main.tsx
@@ -5,11 +5,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 import './styles/globals.scss'
+import AuthProvider from './contexts/AuthContext'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 )
