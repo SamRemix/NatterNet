466cd12d8f6ef6ba8fdb1a53383c2532954c6a3d
SamRemix <s_capron@hotmail.fr>
2023-04-19 12:03:57 +0200

    upgrade errorHandler & prisma version

--- back_/src/middlewares/errorHandler.ts
+++ back_/src/middlewares/errorHandler.ts
@@ -2,13 +2,26 @@ import { Prisma } from '@prisma/client'
 import { Request, Response, NextFunction } from 'express'
 
 const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
-  const { message, code, meta } = err
+  const { code, meta } = err
 
+  // prisma error
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (code === 'P2023') {
+      return res.status(400).json({ code, message: 'Inconsistent column data' })
+    }
+
     return res.status(400).json({ code, message: meta?.cause || 'This error is unhandled' })
+
+    /**
+     * > npx prisma db push
+     * EPERM: operation not permitted
+     * npm uninstall @prisma/client
+     * npm i @prisma/client
+     */
   }
 
-  return res.status(400).json({ message })
+  // express error
+  return res.status(400).json({ err })
 }
 
 export default errorHandler
